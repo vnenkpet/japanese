2aacea35ef097705b8815fcdde3a4d27e24da10e
Petr Vnenk <petr.vnenk@gmail.com>
2018-08-02 21:07:23 +0200

    Enable cluster

--- dictionary-api/src/index.ts
+++ dictionary-api/src/index.ts
@@ -14,6 +14,11 @@ import DbClient from "./services/db";
 
 import { ApolloServer } from "apollo-server";
 
+import * as cluster from "cluster";
+import { cpus } from "os";
+
+const numCPUs = cpus().length;
+
 const schema = buildSchemaSync({
   resolvers: [EntryResolver, KanjiResolver, KanjiDicEntryResolver]
 });
@@ -25,16 +30,33 @@ if (config.NODE_ENV !== "production") {
   );
 }
 
-const server = new ApolloServer({ schema });
-
+// tslint:disable
 async function main() {
-  await DbClient.connect();
-  server.listen(config.PORT, () =>
-    // tslint:disable-next-line
-    console.log(
-      `Dictionary service is running on http://localhost:${config.PORT}`
-    )
-  );
+  if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on("exit", (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+  } else {
+    await DbClient.connect();
+
+    // Workers can share any TCP connection
+    // In this case it is an HTTP server
+    const server = new ApolloServer({ schema });
+    server.listen(config.PORT, () =>
+      // tslint:disable-next-line
+      console.log(
+        `Dictionary service is running on http://localhost:${config.PORT}`
+      )
+    );
+    console.log(`Worker ${process.pid} started`);
+  }
 }
 
 main();
