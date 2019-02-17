2f2316e18f018eb5f76eadbd77da3957dfbaa79e
Petr Vnenk <petr.vnenk@gmail.com>
2019-02-17 10:29:18 +0100

    Bump stringcaster version

--- dictionary-api/src/config.ts
+++ dictionary-api/src/config.ts
@@ -1,20 +1,20 @@
 import * as dotenv from "dotenv";
-import { inject, toNumber, toString } from "typescript-stringcaster";
+import { envVar, toNumber, toString } from "typescript-stringcaster";
 dotenv.config();
 
 const source = process.env;
 
 class Config {
-    @inject({ cast: toString, source })
+    @envVar({ cast: toString, source })
     public MONGODB_URI: string;
 
-    @inject({ cast: toString, source })
+    @envVar({ cast: toString, source })
     public MONGODB_URI_TEST: string;
 
-    @inject({ cast: toNumber, source })
+    @envVar({ cast: toNumber, source })
     public PORT: string;
 
-    @inject({ cast: toString, source, defaultValue: "development" })
+    @envVar({ cast: toString, source, defaultValue: "development" })
     public NODE_ENV: string;
 }
 
